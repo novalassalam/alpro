f56f8d8054c40c79019e329874e9bdb4aa154b7b
noval assalam <naufalassalam@gmail.com>
2024-03-18 08:47:36 +0700

    sort

--- sorting/insertion_sort/insertion_sort/main.cpp
+++ sorting/insertion_sort/insertion_sort/main.cpp
@@ -3,25 +3,34 @@ using namespace std;
 
 void insertionSort(int arr[], int n) {
     int i, key, j;
+    // Loop untuk setiap elemen, dimulai dari elemen kedua (indeks 1)
     for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+        key = arr[i]; // Simpan nilai elemen saat ini dalam key
+        j = i - 1;    // Tentukan indeks sebelumnya dari elemen saat ini
+        // Geser elemen-elemen yang lebih besar dari key ke kanan
         while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
-            j = j - 1;
+            arr[j+1] = arr[j]; // Geser elemen ke kanan
+            j = j - 1;         // Pindah ke elemen sebelumnya
         }
-        arr[j+1] = key;
+        arr[j+1] = key; // Masukkan key ke posisi yang sesuai
+        // Mencetak langkah pengurutan saat ini
         cout << "Step " << i << ": ";
+        // Mencetak array setelah setiap langkah pengurutan
         for (int k = 0; k < n; k++) {
             cout << arr[k] << " ";
         }
-        cout << endl;
+        cout << endl; // Pindah baris
     }
 }
 
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     insertionSort(arr, n);
     return 0;
 }
